04266a501b1934779bec0aac628d9f5e706d4b15
mhx2n <mhdiv2331@gmail.com>
2026-02-10 14:59:21 +0600

    Update extream_exam_bot.py

--- extream_exam_bot.py
+++ extream_exam_bot.py
@@ -63,7 +63,7 @@ from telegram.ext import (
 # =========================================================
 # ✅ HARD-CODED CONFIG
 # =========================================================
-BOT_TOKEN = "8427023407:AAFagu6UcMGJAI2_jJksQTZ0P_Hj9JQTWrI"  # set in Pella Env Vars
+BOT_TOKEN = "8286585007:AAHz1NIOXIbkBATy9qdcrQtHNr0DauL325U"  # set in Pella Env Vars
 OWNER_ID = 8389621809  # your Telegram numeric user id
 
 OWNER_CONTACT = "@Your_Himus"
@@ -4503,5 +4503,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
